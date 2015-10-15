f0b9104154a456c3f82c3c9035b10248239b9273
Odyssefs Aristeidou <airoddyisback@gmail.com>
2015-10-15 23:01:18 +0200

    - Create a asland details template - Css in the islands list and island details - Create a json islands list

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var islandApp = angular.module('islandApp', ['ngRoute', 'islandControllers']);
 islandApp.config(function ($routeProvider) {
     $routeProvider.
         when('/', {
-            templateUrl: 'island.html',
+            templateUrl: 'island-list.html',
             controller: 'IslandsCtrl'
+        }).
+        when('/:islandName', {
+            templateUrl: 'island-details.html',
+            controller: 'IslandDetailCtrl'
         }).
         otherwise({
             redirectTo: '/'
--- controllers.js
+++ controllers.js
@@ -2,7 +2,7 @@
  * Created by odyssefs on 10/3/15.
  */
 
-var islandControllers = angular.module('islandControllers', []);
+var islandControllers = angular.module('islandControllers', ['uiGmapgoogle-maps']);
 
 islandControllers.controller('IslandsCtrl', function ($scope, $http) {
     $http.get('islands.json').success(function (data) {
@@ -10,5 +10,24 @@ islandControllers.controller('IslandsCtrl', function ($scope, $http) {
     });
     $scope.reverse = true;
     $scope.sortField = 'name';
+
+
+});
+
+islandControllers.controller('IslandDetailCtrl', function ($scope, $routeParams, $http) {
+    $http.get('islands.json').success(function (data) {
+        $scope.island = data.filter(function (entry) {
+            return entry.name === $routeParams.islandName;
+        })[0];
+    });
+    $scope.windowOptions = {
+        visible: false
+    };
+    $scope.onClick = function () {
+        $scope.windowOptions.visible = !$scope.windowOptions.visible;
+    };
+    $scope.closeClick = function () {
+        $scope.windowOptions.visible = false;
+    };
 });
 
